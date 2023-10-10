df8457bdab7cc1a0a71f67ecff91fbd7bc0db2c7
= <aymancoom3@gmail.com>
2023-10-10 15:45:23 +0300

    Added Whataspp Part To it

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import shutil
 fileNamesList = e_mainsCreate.createMainsPages(5)
 # 1 Page
 finalFileName = f_merge.combineParts(fileNamesList)
-destination_folder = "C:/Users/BAB AL SAFA/Documents"  
+destination_folder = "C:/Users/BAB AL SAFA/Documents"
 try:
-        source_path = finalFileName
-        destination_path = f"{destination_folder}/{finalFileName}"
-        shutil.move(source_path, destination_path)
-        print(f"Moved {finalFileName} to {destination_folder}")
+    source_path = finalFileName
+    destination_path = f"{destination_folder}/{finalFileName}"
+    shutil.move(source_path, destination_path)
+    print(f"Moved {finalFileName} to {destination_folder}")
 except Exception as e:
-        print(f"Error moving {finalFileName}: {e}")
+    print(f"Error moving {finalFileName}: {e}")
 
 
 # ---------------------------------------------
