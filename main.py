33c598b4648a3182797edc41ab23cde9d651ae3c
= <aymancoom3@gmail.com>
2023-10-10 14:08:44 +0300

    G

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from PartOne import e_mainsCreate, f_merge
-# from PartOne.e_mainsCreate import createMainsPages
-# from PartOne.f_merge import combineParts
+from whatasApp import hideEveryThing, openBrowser, openWhatasppLinkAndSendMSG, handleUPload
 
-fileNamesList = e_mainsCreate.createMainsPages(576)
+# fileNamesList = e_mainsCreate.createMainsPages(576)
 # 4 pages Nearly
 # fileNamesList = e_mainsCreate.createMainsPages(5)
 # 1 Page
 
-finalFileName = f_merge.combineParts(fileNamesList)
-print("finalFileName")
-print(finalFileName)
-# What after the Combinations ?
-# First there are Some Changes
-# For example the Quantitng in the Invoice is Not a Decimal Number and
-# Some Prices Need the Two Decimal Number after the point
-# Second Thing Make The Following :
+# finalFileName = f_merge.combineParts(fileNamesList)
+# print("finalFileName")
+# print(finalFileName)
+
+
+
 # ---------------------------------------------
+hideEveryThing()
+# openBrowser()
+# openWhatasppLinkAndSendMSG()
+# handleUPload()
